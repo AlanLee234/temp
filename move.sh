9bfa164d368b6719fee15fcb21b37461b5edaba7
AlanLee234 <w1299495070@outlook.com>
2022-08-07 15:14:59 +0800

    add sh
    add sh

---
+++ move.sh
@@ -0,0 +1,4 @@
+MV -i ./src/com/alanlee/leetcode/* ../JAVA/src/com/alanlee/leetcode
+MV -i ./src/com/alanlee/nowcoder/* ../JAVA/src/com/alanlee/nowcoder
+MV -i ./src/com/alanlee/real/* ../JAVA/src/com/alanlee/real
+MV -i ./mysql/* ../MYSQL/temp/
