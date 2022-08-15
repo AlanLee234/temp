aecff2aabc5c10b4db28c11305fa39e32a6e54a9
AlanLee234 <w1299495070@outlook.com>
2022-08-15 13:08:37 +0800

    1
    1

--- move.cmd
+++ move.cmd
@@ -1,6 +1,6 @@
 MOVE /-Y .\src\com\alanlee\leetcode\* ..\JAVA\src\com\alanlee\leetcode\
 MOVE /-Y .\src\com\alanlee\nowcoder\* ..\JAVA\src\com\alanlee\nowcoder\
 MOVE /-Y .\src\com\alanlee\real\* ..\JAVA\src\com\alanlee\real\
-MOVE /-Y .\mysql\* ..\MYSQL\temp\
+MOVE /-Y .\mysql\* ..\MYSQL\leetcode\
 
 @REM .\move.cmd
--- move.sh
+++ move.sh
@@ -1,6 +1,6 @@
 MV -i ./src/com/alanlee/leetcode/* ../JAVA/src/com/alanlee/leetcode/
 MV -i ./src/com/alanlee/nowcoder/* ../JAVA/src/com/alanlee/nowcoder/
 MV -i ./src/com/alanlee/real/* ../JAVA/src/com/alanlee/real/
-MV -i ./mysql/* ../MYSQL/
+MV -i ./mysql/* ../MYSQL/leetcode/
 
 # ./move.sh
