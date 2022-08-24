078a3048cbd1ec0c6d5aca7a8dfd5249cfa3ec08
AlanLee234 <w1299495070@outlook.com>
2022-08-24 14:54:01 +0800

    up
    824

--- move.sh
+++ move.sh
@@ -1,6 +1,6 @@
 MV -i ./src/com/alanlee/leetcode/* ../JAVA/src/com/alanlee/leetcode/
 MV -i ./src/com/alanlee/nowcoder/* ../JAVA/src/com/alanlee/nowcoder/
 MV -i ./src/com/alanlee/real/* ../JAVA/src/com/alanlee/real/
-MV -i ./mysql/* ../MYSQL/leetcode/
+MV -i ./MYSQL/* ../MYSQL/nowcoder/
 
 # ./move.sh
