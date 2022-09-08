2331c90beb0cf7ee0d31049a7cc68cd759d21c35
AlanLee234 <w1299495070@outlook.com>
2022-09-08 22:23:39 +0800

    templete
    templete

--- src/App.java
+++ src/App.java
@@ -1,12 +1,24 @@
 import java.util.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {    
+        /*
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextInt()) {
-        // String s = sc.nextLine();
-        // System.out.println(Integer.parseInt(s.substring(2,s.length()),16));
+            String s = sc.nextLine();
+            System.out.println(Integer.parseInt(s.substring(2,s.length()),16));
         }
         sc.close();
+        */      
+    }
+
+    public static int[][] inputMatrix(Scanner sc, int m, int n) {
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        return matrix;
     }
 }
