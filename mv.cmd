558e7bb41e771bfacf98f61394508817857de486
AlanLee234 <w1299495070@outlook.com>
2022-09-11 22:06:43 +0800

    q
    q

--- mv.cmd
+++ mv.cmd
@@ -2,5 +2,6 @@ MOVE /-Y .\src\leetcode\* ..\JAVA\src\leetcode\
 MOVE /-Y .\src\nowcoder\* ..\JAVA\src\nowcoder\
 MOVE /-Y .\src\real\* ..\JAVA\src\real\
 MOVE /-Y .\MYSQL\nowcoder\* ..\MYSQL\nowcoder\
+MOVE /-Y .\MYSQL\real\* ..\MYSQL\real\
 
 @REM .\mv.cmd
--- mv.sh
+++ mv.sh
@@ -2,5 +2,6 @@ MV -i ./src/leetcode/* ../JAVA/src/leetcode/
 MV -i ./src/nowcoder/* ../JAVA/src/nowcoder/
 MV -i ./src/real/* ../JAVA/src/real/
 MV -i ./MYSQL/nowcoder/* ../MYSQL/nowcoder/
+MV -i ./MYSQL/real/* ../MYSQL/real/
 
 # ./mv.sh
