66f2fc2fb168115bf72e1a95f84ac96fb2156b48
AlanLee234 <w1299495070@outlook.com>
2022-10-01 19:15:45 +0800

    py
    py

--- mv.cmd
+++ mv.cmd
@@ -1,7 +1,10 @@
 MOVE /-Y .\src\leetcode\* ..\JAVA\src\leetcode\
 MOVE /-Y .\src\nowcoder\* ..\JAVA\src\nowcoder\
 MOVE /-Y .\src\real\* ..\JAVA\src\real\
-MOVE /-Y .\MYSQL\nowcoder\* ..\MYSQL\nowcoder\
-MOVE /-Y .\MYSQL\real\* ..\MYSQL\real\
+
+MOVE /-Y .\MySQL\nowcoder\* ..\MySQL\nowcoder\
+MOVE /-Y .\MySQL\real\* ..\MySQL\real\
+
+MOVE /-Y .\Python\leetcode\* ..\Python\leetcode\
 
 @REM .\mv.cmd
--- mv.sh
+++ mv.sh
@@ -1,7 +1,10 @@
 MV -i ./src/leetcode/* ../JAVA/src/leetcode/
 MV -i ./src/nowcoder/* ../JAVA/src/nowcoder/
 MV -i ./src/real/* ../JAVA/src/real/
-MV -i ./MYSQL/nowcoder/* ../MYSQL/nowcoder/
-MV -i ./MYSQL/real/* ../MYSQL/real/
+
+MV -i ./MySQL/nowcoder/* ../MySQL/nowcoder/
+MV -i ./MySQL/real/* ../MySQL/real/
+
+MV -i ./Python/leetcode/* ../Python/leetcode/
 
 # ./mv.sh
