67a3347dc88cf1a0ac19751bed0bbe11a3f57810
AlanLee234 <w1299495070@outlook.com>
2023-08-06 20:22:19 +0800

    修改格式

--- src/Solution.java
+++ src/Solution.java
@@ -2,13 +2,6 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        while (sc.hasNextInt()) {
-            String str = sc.nextLine();
-            String[] s = str.split(" ");
-            System.out.println(Integer.parseInt(str.substring(2, str.length()), 16));
-        }
-        sc.close();
+        
     }
 }
